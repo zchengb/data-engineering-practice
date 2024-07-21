f4275d5af1ba291f012ca5a0f307c6e53c658cd4
xiaobin <xiaobin.zheng@thoughtworks.com>
2024-07-21 12:57:22 +0800

    feat: add fetch API of statistic data.

--- airflow/dags/process_hard_drive_data_dag.py
+++ airflow/dags/process_hard_drive_data_dag.py
@@ -8,7 +8,6 @@ from clickhouse_driver import Client
 import pandas as pd
 from pyspark.sql.types import StructType
 
-
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -40,8 +39,9 @@ def create_spark_session():
         .master('spark://spark-master:7077') \
         .getOrCreate()
 
-def read_and_transform_data(spark):
-    df = spark.read.csv('file:///opt/airflow/datalake/mini_all_data.csv', header=True)
+def read_and_transform_data(**kwargs):
+    spark = create_spark_session()
+    df = spark.read.csv('file:///opt/airflow/datalake/all_data.csv', header=True)
 
     df_transformed = df.withColumn("brand",
         when(col("model").startswith("CT"), "Crucial")
@@ -53,28 +53,36 @@ def read_and_transform_data(spark):
         .otherwise("Others")
     )
 
-    return df_transformed
+    kwargs['ti'].xcom_push(key='df_transformed', value=df_transformed.toJSON().collect())
+    spark.stop()
 
-def get_daily_summary(df_transformed):
-    return df_transformed.groupBy("date").agg(
+def get_daily_summary(**kwargs):
+    spark = create_spark_session()
+    df_transformed_json = kwargs['ti'].xcom_pull(key='df_transformed', task_ids='transform_data')
+    df_transformed = spark.read.json(spark.sparkContext.parallelize(df_transformed_json))
+
+    daily_summary = df_transformed.groupBy("date").agg(
         count("serial_number").alias("drive_count"),
         sum(when(col("failure") == "1", 1).otherwise(0)).alias("drive_failures")
     )
 
-def get_yearly_summary(df_transformed):
-    return df_transformed.groupBy(year("date").alias("year")).agg(
-        count("serial_number").alias("drive_count"),
-        sum(when(col("failure") == "1", 1).otherwise(0)).alias("drive_failures")
-    )
+    kwargs['ti'].xcom_push(key='daily_summary', value=daily_summary.toJSON().collect())
+    spark.stop()
 
-def write_to_clickhouse(df_transformed, table_name, order_by):
-    pandas_df = df_transformed.toPandas()
+def write_to_clickhouse(**kwargs):
+    table_name = kwargs['table_name']
+    order_by = kwargs['order_by']
+    df_json = kwargs['ti'].xcom_pull(key=kwargs['df_key'], task_ids=kwargs['df_task_id'])
+
+    spark = create_spark_session()
+    df = spark.read.json(spark.sparkContext.parallelize(df_json))
+    pandas_df = df.toPandas()
     pandas_df = pandas_df.fillna('')
 
     client = Client(host='clickhouse-server', port=9000, user='default', password='', database='default')
     client.execute(f'DROP TABLE IF EXISTS {table_name}')
 
-    columns = ", ".join([f"{col.name} {spark_to_clickhouse_type_mapping[col.dataType.simpleString()]}" for col in df_transformed.schema])
+    columns = ", ".join([f"{col.name} {spark_to_clickhouse_type_mapping[col.dataType.simpleString()]}" for col in df.schema])
 
     initialize_sql = f'''
     CREATE TABLE IF NOT EXISTS {table_name} (
@@ -84,25 +92,57 @@ def write_to_clickhouse(df_transformed, table_name, order_by):
     '''
     client.execute(initialize_sql)
 
-    data = pandas_df.to_dict('records')
-    client.execute(f'INSERT INTO {table_name} VALUES', data)
-
-def hard_drive_data_transform(**kwargs):
-    spark = create_spark_session()
-    df_transformed = read_and_transform_data(spark)
-
-    daily_summary = get_daily_summary(df_transformed)
-    yearly_summary = get_yearly_summary(df_transformed)
-
-    write_to_clickhouse(daily_summary, 'daily_summary', 'date')
-    write_to_clickhouse(yearly_summary, 'yearly_summary', 'year')
-
+    data = [tuple(row) for row in pandas_df.itertuples(index=False, name=None)]
+    column_names = ", ".join(pandas_df.columns)
+    client.execute(f'INSERT INTO {table_name} ({column_names}) VALUES', data)
     spark.stop()
 
+def get_yearly_summary(**kwargs):
+    spark = create_spark_session()
+    df_transformed_json = kwargs['ti'].xcom_pull(key='df_transformed', task_ids='transform_data')
+    df_transformed = spark.read.json(spark.sparkContext.parallelize(df_transformed_json))
+
+    yearly_summary = df_transformed.groupBy(year("date").alias("year")).agg(
+        count("serial_number").alias("drive_count"),
+        sum(when(col("failure") == "1", 1).otherwise(0)).alias("drive_failures")
+    )
+
+    kwargs['ti'].xcom_push(key='yearly_summary', value=yearly_summary.toJSON().collect())
+    spark.stop()
 
 transform_data = PythonOperator(
     task_id='transform_data',
-    python_callable=hard_drive_data_transform,
+    python_callable=read_and_transform_data,
+    dag=dag,
+    provide_context=True
+)
+
+statistic_daily_data = PythonOperator(
+    task_id='statistic_daily_data',
+    python_callable=get_daily_summary,
+    dag=dag,
+    provide_context=True
+)
+
+write_daily_data_to_clickhouse = PythonOperator(
+    task_id='write_daily_data_to_clickhouse',
+    python_callable=write_to_clickhouse,
+    op_kwargs={'table_name': 'daily_summary', 'order_by': 'date', 'df_key': 'daily_summary', 'df_task_id': 'statistic_daily_data'},
+    dag=dag,
+    provide_context=True
+)
+
+statistic_yearly_data = PythonOperator(
+    task_id='statistic_yearly_data',
+    python_callable=get_yearly_summary,
+    dag=dag,
+    provide_context=True
+)
+
+write_yearly_data_to_clickhouse = PythonOperator(
+    task_id='write_yearly_data_to_clickhouse',
+    python_callable=write_to_clickhouse,
+    op_kwargs={'table_name': 'yearly_summary', 'order_by': 'year', 'df_key': 'yearly_summary', 'df_task_id': 'statistic_yearly_data'},
     dag=dag,
     provide_context=True
 )
@@ -117,4 +157,4 @@ end = DummyOperator(
     dag=dag
 )
 
-start >> transform_data >> end
+start >> transform_data >> statistic_daily_data >> write_daily_data_to_clickhouse >> statistic_yearly_data >> write_yearly_data_to_clickhouse >> end
--- app/backend/app.py
+++ app/backend/app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
+from clickhouse_driver import Client
+
 app = Flask(__name__)
 
+client = Client(host='localhost', port=9000, user='default', password='', database='default')
+
+
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+
+
+@app.route('/api/daily_summary')
+def get_daily_summary():
+    query = "SELECT date, drive_count, drive_failures FROM daily_summary"
+    data = client.execute(query)
+
+    result = [{"date": row[0], "drive_count": row[1], "drive_failures": row[2]} for row in data]
+
+    return jsonify(result)
+
+
+@app.route('/api/yearly_summary')
+def get_yearly_summary():
+    query = "SELECT year, drive_count, drive_failures FROM yearly_summary"
+    data = client.execute(query)
+
+    result = [{"year": row[0], "drive_count": row[1], "drive_failures": row[2]} for row in data]
+
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run()
